a5195b2e953fd3ac15784117abeaca05b134f0c0
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-02-23 22:32:12 +0100

    make the design responsive: adding hamburger menu

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,8 +5,8 @@ import Logo from '../assets/app-logo-white.png'
 function Header() {
   return (
     <header>
+        <img src={Logo} alt='logo' className='logo'/>
         <div className='header-container'>
-            <img src={Logo} alt='logo'/>
             <nav className="navbar">
                 <ul>
                     <li><a href='#'>Home</a></li>
@@ -15,6 +15,12 @@ function Header() {
                     <li><a href='#'>Contact</a></li>
                 </ul>
             </nav>
+            <input type='checkbox' href='#' className='checkbox' id='menu-open'/>
+            <label htmlFor='menu-open' className='menu-open-button'>
+              <span className='line-1'></span>
+              <span className='line-2'></span>
+              <span className='line-3'></span>
+            </label>
         </div>
     </header>
   )
